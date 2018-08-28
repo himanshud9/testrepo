2f79a0ee85028a5f66cb367d95aaee7fac50bf18
himanshud9 <himanshud987@gmail.com>
2018-08-28 07:26:20 +0530

    new script file

---
+++ app.sh
@@ -0,0 +1,147 @@
+ curl -H "Content-type: application/json" -H "Authorization: Bearer  4e8078c5-3b52-45ed-9421-2b3f6b66a642" -X POST "http://euapi.bidgely.com/2.1/users/14a6d157-07f2-4cc2-9b73-3f4c496b81c2/homes/1/appliances" -d "
+[
+    {
+      \"appID\": 1,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 2,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 6,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 9,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 16,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 18,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 22,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 23,
+      \"number\": -1,
+      \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 24,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 25,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 28,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 29,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 30,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 31,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 36,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 40,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 44,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 58,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 60,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 61,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 62,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 63,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 64,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    },
+    {
+      \"appID\": 65,
+      \"number\": -1,
+     \"type\": null,
+      \"size\": null
+    }
+    ]"
